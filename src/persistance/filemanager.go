37ef0cf517c094657d1107edefcac22f94058fcb
vlado-ma <marinkovic@founder.org>
2017-06-17 17:12:02 +0200

    Update function added to Persistance

--- src/persistance/filemanager.go
+++ src/persistance/filemanager.go
@@ -9,10 +9,13 @@ import (
 	"strings"
 	"errors"
 	"path"
+	"sync"
+	"bytes"
 )
 
 type FileManager interface {
 	Write(command Command) error
+	Update(command Command) error
 	Read(query Query) (string, error)
 	ReadFile() ([]byte, error)
 	Close() error
@@ -21,6 +24,9 @@ type FileManager interface {
 type fileManager struct{
 	file *os.File
 }
+
+var mutex = &sync.Mutex{}
+var pos int64
 
 func NewFileManager(pathToDir string, topic string) FileManager{
 	pathToFile := path.Clean(path.Join( pathToDir, topic))
@@ -35,6 +41,8 @@ func NewFileManager(pathToDir string, topic string) FileManager{
 }
 
 func (fm *fileManager) Write(command Command) error {
+	mutex.Lock()
+	defer mutex.Unlock()
 	w := bufio.NewWriter(fm.file)
 	size, err := w.WriteString(command.Key.String() + ":" +command.Text+"\n")
 	if err != nil {
@@ -42,6 +50,67 @@ func (fm *fileManager) Write(command Command) error {
 		log.Fatal(err)
 	}
 	w.Flush()
+	return err
+}
+
+func (fm *fileManager) Update(command Command) error {
+	mutex.Lock()
+	defer mutex.Unlock()
+	fileHandle, _ := os.OpenFile(fm.file.Name(), os.O_RDWR, 0777)
+	defer fileHandle.Close()
+	scanner := bufio.NewScanner(fileHandle)
+	splitFunc := newSplitFunc()
+	scanner.Split(splitFunc)
+	for scanner.Scan() {
+		oldBytesText := scanner.Bytes()
+		text := string(oldBytesText)
+		if strings.Contains(text, command.Key.String()) {
+			newText := command.Key.String() + ":" + command.Text+"\n"
+			newBytesText := []byte(newText)
+			diff := len(oldBytesText) - len(newBytesText)
+			// fits the message size - performs replacement
+			if diff >= 0 {
+				var additional string
+				for i:=0; i<=diff; i++ {
+					additional += " "
+				}
+				newBytesText = []byte(command.Key.String() + ":" + command.Text+additional+"\n")
+				newBytesText = bytes.Replace(oldBytesText, oldBytesText, newBytesText, -1)
+				fileHandle.Seek(0, 0)
+				n, err := fileHandle.WriteAt(newBytesText, pos-int64(len(oldBytesText)))
+				if err != nil{
+					fmt.Println("Update failed. Writen bytes: ",n)
+					fmt.Println(err)
+					return err
+				}
+			} else {
+				// new message larger than old one - delete and append
+				var emptyLine string
+				for i:=0; i<len(oldBytesText); i++ {
+					emptyLine += " "
+				}
+				emptyLineBytes := []byte(emptyLine)
+				fileHandle.Seek(0, 0)
+				n, errDelete := fileHandle.WriteAt(emptyLineBytes, pos-int64(len(oldBytesText)))
+				if errDelete != nil{
+					fmt.Println("Update failed. Writen bytes: ",n)
+					fmt.Println(errDelete)
+					return errDelete
+				}
+				fileHandle.Seek(0, 2) //EOF
+				n, errAppend := fileHandle.WriteString(newText)
+				if errAppend != nil{
+					fmt.Println("Update failed. Writen bytes: ",n)
+					fmt.Println(errAppend)
+					return errAppend
+				}
+			}
+
+
+			return nil
+		}
+	}
+	err := errors.New("Item not found")
 	return err
 }
 
@@ -81,4 +150,18 @@ func (fm *fileManager) Close() error{
 		return err
 	}
 	return err
+}
+
+func newSplitFunc() bufio.SplitFunc {
+	var n int64
+	return func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+		advance, token, err = bufio.ScanLines(data, atEOF)
+		if err == nil && (atEOF || advance > len(token)) {
+			// We found the end of a line
+			pos = n + int64(len(token))
+			token = append(([]byte)(nil), token...)
+		}
+		n += int64(advance)
+		return
+	}
 }
--- src/persistance/persistance_test.go
+++ src/persistance/persistance_test.go
@@ -7,6 +7,7 @@ import (
 	"path"
 	"path/filepath"
 	"fmt"
+	"strings"
 )
 
 var pathToDir string
@@ -15,15 +16,14 @@ var fm FileManager
 
 func TestMain(m *testing.M) {
 	//setup
-	path, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		fmt.Println("Path of working directory not found.")
-	}
-	pathToDir = path
-	fm = NewFileManager(pathToDir, topic)
-	os.Exit(m.Run())
+	setUp()
+
+	retCode := m.Run()
+
 	//teardown
 	cleanUp()
+
+	os.Exit(retCode)
 }
 
 func TestFileManager_Write(t *testing.T) {
@@ -33,6 +33,90 @@ func TestFileManager_Write(t *testing.T) {
 	}
 	err := fm.Write(cmd)
 	if err != nil {
+		t.Fail()
+	}
+}
+
+func TestFileManager_UpdateShort(t *testing.T) {
+	keyForUpdate := uuid.New()
+	cmd01 := Command{
+		Key: uuid.New(),
+		Text: "This is testing message for persistance: updatingShort01.",
+	}
+	cmd02 := Command{
+		Key: keyForUpdate,
+		Text: "This is testing message for persistance: updatingShort02.",
+	}
+	cmd03 := Command{
+		Key: uuid.New(),
+		Text: "This is testing message for persistance: updatingShort03.",
+	}
+	err01 := fm.Write(cmd01)
+	err02 := fm.Write(cmd02)
+	err03 := fm.Write(cmd03)
+	if err01 != nil || err02 != nil || err03 != nil {
+		t.Fail()
+	}
+	updateCmd := Command{
+		Key: keyForUpdate,
+		Text: "Text is changed.",
+	}
+	err := fm.Update(updateCmd)
+	if err != nil {
+		t.Fail()
+	}
+
+	query := Query{
+		Key: keyForUpdate,
+	}
+	data, err := fm.Read(query)
+	fmt.Println(data)
+	fmt.Println(updateCmd.Text)
+	if err != nil {
+		t.Fail()
+	}
+	if !strings.Contains(data, updateCmd.Text) {
+		t.Fail()
+	}
+}
+
+func TestFileManager_UpdateLong(t *testing.T) {
+	keyForUpdate := uuid.New()
+	cmd01 := Command{
+		Key: uuid.New(),
+		Text: "This is testing message for persistance: updating01.",
+	}
+	cmd02 := Command{
+		Key: keyForUpdate,
+		Text: "This is testing message for persistance: updating02.",
+	}
+	cmd03 := Command{
+		Key: uuid.New(),
+		Text: "This is testing message for persistance: updating03.",
+	}
+	err01 := fm.Write(cmd01)
+	err02 := fm.Write(cmd02)
+	err03 := fm.Write(cmd03)
+	if err01 != nil || err02 != nil || err03 != nil {
+		t.Fail()
+	}
+	updateCmd := Command{
+		Key: keyForUpdate,
+		Text: "Message is changed with this new content. Blah blah blah blah blah blah blah blah blah blah blah blah",
+	}
+	err := fm.Update(updateCmd)
+	if err != nil {
+		t.Fail()
+	}
+
+	query := Query{
+		Key: keyForUpdate,
+	}
+	data, err := fm.Read(query)
+	if err != nil {
+		t.Fail()
+	}
+	if data != updateCmd.Text {
 		t.Fail()
 	}
 }
@@ -73,6 +157,16 @@ func TestFileManager_ReadFile(t *testing.T) {
 	if len(data) <= 0 {
 		t.Fail()
 	}
+}
+
+func setUp(){
+	path, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		fmt.Println("Path of working directory not found.")
+	}
+	pathToDir = "D:\\"//+" "+path
+	fmt.Println(path)
+	fm = NewFileManager(pathToDir, topic)
 }
 
 func cleanUp(){
