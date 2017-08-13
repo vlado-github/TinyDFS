9ddbd97f7e3ae3148803198931e3f23f4e921a30
just-vlado <vladimir@sevenlab.org>
2017-08-13 11:46:57 +0200

    libs refactored and moved

--- TinyDFS/queue_app/main.go
+++ TinyDFS/queue_app/main.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-	"messaging"
 	"fmt"
+	"messaging"
 )
 
 func main() {
 	printWelcome()
 	var connParams = messaging.ConnParams{
-		Ip:"localhost",
-		Port:"3333",
-		Protocol:"tcp",
+		Ip:       "localhost",
+		Port:     "3333",
+		Protocol: "tcp",
 	}
 	var queue = messaging.NewQueue(connParams)
 	queue.Run()
 }
 
-func printWelcome(){
+func printWelcome() {
 	fmt.Println("*********************")
 	fmt.Println("Welcome to TinyDFS")
 	fmt.Println("*********************")
-	fmt.Println("\n")
+	//fmt.Println("\n")
 	fmt.Println("Message queue started!")
-	fmt.Println("\n")
+	//fmt.Println("\n")
 }
